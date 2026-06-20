b589c0eff3f2caadf1d38ee863df853f54a792f8
iamnothing-hub <amitrrrvaa0003@gmail.com>
2026-06-21 01:36:40 +0530

    Integration of reset-password.html

--- src/main/java/com/irctc/userservice/controller/ResetPasswordController.java
+++ src/main/java/com/irctc/userservice/controller/ResetPasswordController.java
@@ -1,20 +1,25 @@
 package com.irctc.userservice.controller;
 
+import com.irctc.userservice.dto.request.ResetPasswordRequest;
 import com.irctc.userservice.exception.ResourceNotFoundException;
 import com.irctc.userservice.service.PasswordResetTokenService;
+import com.irctc.userservice.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class ResetPasswordController {
 
     private final PasswordResetTokenService passwordResetTokenService;
+    private final UserService userService;
 
     @GetMapping("/reset-password")
     public String showResetPasswordPage(
@@ -25,7 +30,10 @@ public class ResetPasswordController {
 
             passwordResetTokenService.validateToken(token);
 
-            model.addAttribute("token", token);
+            model.addAttribute("resetPasswordRequest",
+                    new ResetPasswordRequest());
+
+//            model.addAttribute("token", token);
 
             return "reset-password";
 
@@ -36,6 +44,20 @@ public class ResetPasswordController {
 
         }
 
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPassword(
+            @Valid @ModelAttribute("resetPasswordRequest") ResetPasswordRequest resetPasswordRequest,
+            BindingResult bindingResult
+    ){
+        log.info("reset password reques: {}", resetPasswordRequest);
+        log.info("binding result: {}", bindingResult);
+        if(bindingResult.hasErrors()){
+            return "reset-password";
+        }
+        userService.setPassword(resetPasswordRequest);
+        return "success";
     }
 
 }
